0718d100efbbbb45e60268d2af0cdfc9ed089068
Rayen1a <raihan01426@gmail.com>
2025-03-25 00:47:58 +0700

    Memasukkan maksimal 20 array

--- file1.cpp
+++ file1.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 using namespace std;
 
-
 int arr [20]; // Membuat Array dengan panjang data 20
 int n; // Membuat Variable inputan n
+
 void input( )
-
-
 { // Procedure Input
-
 while (true)
-
-
+{
 cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan jumlahh element Array
-
 cin >> n; // memanggil variable inputan n
+
+if (n <= 20)
+{ // Membuat Kondisi n tidak lebih dari 20
+break;
+}
+else
+{
+cout << "\nArray yang anda masukkan maksimal 20 Elemen. \n"; // Menampilkan Pesan jika data lebih dari 20
+}
+}
+cout << endl; // Membuat jarak per baris program
+cout << " ====================== " << endl; // Membuat tampilan susunan data element array
+cout << "Masukkan Element Array" << endl;
+cout << " ========= " << endl;
